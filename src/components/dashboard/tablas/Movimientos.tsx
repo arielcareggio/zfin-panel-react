384e21aad6145d2ce8e6caed0abec1e4a86b8705
Ariel <arielcar04@gmail.com>
2023-09-30 17:44:00 -0300

    Tabla mejorada visualmente

--- src/components/dashboard/tablas/Movimientos.tsx
+++ src/components/dashboard/tablas/Movimientos.tsx
@@ -36,7 +36,7 @@ function Movimientos() {
     putShowing(true);
     setTimeout(() => {
       putShowing(false);
-    }, segundos*1000); // Ocultar después de 5 segundos
+    }, segundos * 1000); // Ocultar después de 5 segundos
   };
 
   const overlayClass = isShowing ? 'overlay show' : 'overlay';
@@ -54,7 +54,7 @@ function Movimientos() {
       eliminarMovimiento(movimientoIdToDelete);
       setMovimientoIdToDelete(null); // Reinicia el estado
     }
-    
+
   };
 
   const handleCancel = () => {
@@ -89,7 +89,7 @@ function Movimientos() {
         let datos: data = response.data ? response.data : '';
         if (datos.data.error) {
           setMovimientos(movimientos);
-          showMessage('No se pudo eliminar el registro: '+ datos.data.error, 'danger', 5);
+          showMessage('No se pudo eliminar el registro: ' + datos.data.error, 'danger', 5);
         } else {
           eventoConfirmado(datos.data.message);
         }
@@ -103,78 +103,78 @@ function Movimientos() {
   };
 
   return (
-    <div className="bg-fondo-cuenta-principal h-auto rounded-xl text-sm divide-y-2 divide-fondo-cuenta shadow-lg shadow-slate-500 hover:shadow-slate-700">
-
+    <div>
       <div className={overlayClass}>
         <Success texto={texto} tipo={tipo} />
       </div>
+      <div className="bg-fondo-cuenta-principal h-auto rounded-xl text-sm divide-y-2 divide-fondo-cuenta shadow-lg shadow-slate-500 hover:shadow-slate-700">
+        <div>
+          <h1 className="text-center tracking-widest font-bold text-white">MOVIMIENTOS</h1>
+        </div>
 
-      <div>
-        <h1 className="text-center tracking-widest font-bold text-white">MOVIMIENTOS</h1>
-      </div>
-
-      <div className="overflow-x-auto">
+        <div className="overflow-x-auto">
         <table className="min-w-full divide-y-2 divide-gray-200  text-sm text-center border-b border-gray-700 ">
           <thead className="ltr:text-left rtl:text-right text-white">
-            <tr>
-              <th className="whitespace-nowrap px-4 py-2 font-medium">Fecha</th>
-              <th className="whitespace-nowrap px-4 py-2 font-medium">Monto</th>
-              <th className="whitespace-nowrap px-4 py-2 font-medium">Movimiento</th>
-              <th className="whitespace-nowrap px-4 py-2 font-medium">Cuenta</th>
-              <th className="whitespace-nowrap px-4 py-2 font-medium">Comentario</th>
-              <th className="px-4 py-2"></th>
-            </tr>
-          </thead>
-
-          <tbody className="divide-y divide-gray-200 bg-fondo-cuenta">
-
-            {movimientos.map((movimiento) => (
-              <tr className="hover:bg-fondo-tabla-hover" id={movimiento.id + ''} key={movimiento.id}>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-800">{movimiento.fecha}</td>
-                <td className={`whitespace-nowrap px-4 py-2 font-medium ${(movimiento.monto < 0) ? "text-red-800" : "text-green-800"}`}>{movimiento.monto}</td>
-                <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-800">{movimiento.name_movimiento_tipo}</td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-800">{movimiento.name_banco_cuenta}</td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-800">{movimiento.comentario}</td>
-                <td className="whitespace-nowrap px-4 py-2">
-                  <div className="grid gap-2 mb-2 mt-2 grid-cols-2">
-
-                    <span
-                      className="w-10 h-10 text-sm text-white bg-blueGray-200 inline-flex items-center justify-center rounded-full cursor-pointer"
-                      onClick={editarMovimiento}
-                    >
-                      <img
-                        alt="User"
-                        className=""
-                        width={16}
-                        src="src/assets/pen.svg"
-                      />
-                    </span>
-
-                    <span
-                      className="w-10 h-10 text-sm text-white bg-blueGray-200 inline-flex items-center justify-center rounded-full cursor-pointer"
-                      onClick={() => {
-                        if (!showConfirmation) {
-                          console.log('paso');
-                          setMovimientoIdToDelete(movimiento.id);
-                          setShowConfirmation(true);
-                        }}}
-                    >
-                      <img
-                        alt="User"
-                        width={16}
-                        src="src/assets/trash.svg"
-                      />
-                    </span>
-                  </div>
-                </td>
+              <tr>
+                <th className="whitespace-nowrap px-4 py-2 font-medium">Fecha</th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium">Monto</th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium">Movimiento</th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium">Cuenta</th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium">Comentario</th>
+                <th className="px-4 py-2 "></th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
 
-      </div>
+            <tbody className="divide-y divide-gray-200 bg-fondo-cuenta">
 
-      {/* Modal de confirmación para eliminar */}
+              {movimientos.map((movimiento) => (
+                <tr className="hover:bg-fondo-tabla-hover" id={movimiento.id + ''} key={movimiento.id}>
+                  <td className="whitespace-nowrap px-4 py-2 text-gray-800">{movimiento.fecha}</td>
+                  <td className={`whitespace-nowrap px-4 py-2 font-medium ${(movimiento.monto < 0) ? "text-red-800" : "text-green-800"}`}>{movimiento.monto}</td>
+                  <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-800">{movimiento.name_movimiento_tipo}</td>
+                  <td className="whitespace-nowrap px-4 py-2 text-gray-800">{movimiento.name_banco_cuenta}</td>
+                  <td className="whitespace-pre-wrap px-4 py-2 text-gray-800">{movimiento.comentario}</td>
+
+                  <td className="whitespace-nowrap px-4 py-2">
+                    <div>
+                      <span
+                        className="w-10 h-10 text-sm text-white bg-blueGray-200 inline-flex items-center justify-center rounded-full cursor-pointer"
+                        onClick={editarMovimiento}
+                      >
+                        <img
+                          alt="User"
+                          className=""
+                          width={16}
+                          src="src/assets/pen.svg"
+                        />
+                      </span>
+
+                      <span
+                        className="w-10 h-10 text-sm text-white bg-blueGray-200 inline-flex items-center justify-center rounded-full cursor-pointer"
+                        onClick={() => {
+                          if (!showConfirmation) {
+                            console.log('paso');
+                            setMovimientoIdToDelete(movimiento.id);
+                            setShowConfirmation(true);
+                          }
+                        }}
+                      >
+                        <img
+                          alt="User"
+                          width={16}
+                          src="src/assets/trash.svg"
+                        />
+                      </span>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+        </div>
+
+        {/* Modal de confirmación para eliminar */}
         <Confirmation
           isOpen={showConfirmation}
           onConfirm={handleConfirm}
@@ -188,6 +188,7 @@ function Movimientos() {
             isDisabled: true,
           }}
         />
+      </div>
     </div>
   )
 }
